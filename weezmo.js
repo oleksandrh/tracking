41df6e61f8d71910ba8434ed116713ed9767128b
oleksandrh <alex.hrebeniuk@gmail.com>
2020-10-09 14:12:15 +0300

    moved pixel out of weezmo script

--- weezmo.js
+++ weezmo.js
@@ -6,27 +6,16 @@ try {
     ga('wtracker.send', 'pageview');
 }
 catch (err) {
+    console.log(err);
 }
 
 try {
-    !function (f, b, e, v, n, t, s) {
-        if (f.fbq) return; n = f.fbq = function () {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-        n.queue = []; t = b.createElement(e); t.async = !0;
-        t.src = v; s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '325104831779517');
-    fbq('trackSingle', '325104831779517', 'PageView');
     fbq('trackSingleCustom', '325104831779517', '53892bb6-f8dd-4509-a8ad-fef13c9336bf', {
         wid: '5A89Vl3neQL6abPu6Jx93xkDSvat1U'
     });
 }
 catch (err) {
+    console.log(err);
 }
 
 try {
@@ -36,4 +25,5 @@ try {
     });
 }
 catch (err) {
+    console.log(err);
 }
