da166b116e655c9507b74f6a1894100c62d5ce8d
oleksandrh <alex.hrebeniuk@gmail.com>
2020-12-10 10:39:32 +0200

    Update weezmo.js

--- weezmo.js
+++ weezmo.js
@@ -23,3 +23,42 @@ try {
 catch (err) {
     console.log(err);
 }
+
+function setCookie(name, value, props) {
+
+    props = props || {}
+
+    var exp = props.expires
+
+    if (typeof exp == "number" && exp) {
+
+        var d = new Date()
+
+        d.setTime(d.getTime() + exp*1000)
+
+        exp = props.expires = d
+
+    }
+
+    if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
+
+    value = encodeURIComponent(value)
+
+    var updatedCookie = name + "=" + value
+
+    for(var propName in props){
+
+        updatedCookie += "; " + propName
+
+        var propValue = props[propName]
+
+        if(propValue !== true){ updatedCookie += "=" + propValue }
+    }
+
+    document.cookie = updatedCookie
+}
+
+if(!getCookie("_wid"))
+{
+    setCookie("_wid", '5A89Vl3neQL6abPu6Jx93xkDSvat1U', {})
+}
